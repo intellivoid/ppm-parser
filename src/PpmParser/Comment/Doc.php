1e72859db74ea9e97f0623041243971b285b0d6b
netkas <netkas@intellivoid.info>
2020-05-04 23:50:39 -0400

    Initial Commit

---
+++ src/PpmParser/Comment/Doc.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace PpmParser\Comment;
+
+class Doc extends \PpmParser\Comment
+{
+}
---
+++ src/PpmParser/ConstExprEvaluationException.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace PpmParser;
+
+class ConstExprEvaluationException extends \Exception
+{}
